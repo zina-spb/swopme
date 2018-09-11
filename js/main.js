5a71cfa80e2787ab075d37882d6cd8f3f122f738
Zina <alimuradoff@gmail.com>
2018-09-11 15:27:34 +0300

    1
    1

--- js/main.js
+++ js/main.js
@@ -6,3 +6,25 @@ $(document).ready(function(){
     $('.menu-mob').toggleClass('menu-on');
   })
 })
+
+jQuery(document).ready(function($){
+	//open popup
+	$('.button').on('click', function(event){
+		event.preventDefault();
+		$('.cd-popup').addClass('is-visible');
+	});
+
+	//close popup
+	$('.cd-popup').on('click', function(event){
+		if( $(event.target).is('.cd-popup-close') || $(event.target).is('.cd-popup') ) {
+			event.preventDefault();
+			$(this).removeClass('is-visible');
+		}
+	});
+	//close popup when clicking the esc keyboard button
+	$(document).keyup(function(event){
+    	if(event.which=='27'){
+    		$('.cd-popup').removeClass('is-visible');
+	    }
+    });
+});
